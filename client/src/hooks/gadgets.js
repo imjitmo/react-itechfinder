1cdcd7bb3230a5a1fb2dc9275e9fad19ad9fe789
jitmo_0421 <jerram0421@gmail.com>
2024-02-27 11:19:43 +0800

    frontend: fixed shop setup style

---
+++ client/src/hooks/gadgets.js
@@ -0,0 +1,28 @@
+const gadgets = [
+  {
+    label: 'Smartphone',
+    value: 'smartphone',
+  },
+  {
+    label: 'Apple Phone',
+    value: 'applephone',
+  },
+  {
+    label: 'Laptop',
+    value: 'laptop',
+  },
+  {
+    label: 'Mac',
+    value: 'mac',
+  },
+  {
+    label: 'Desktop',
+    value: 'desktop',
+  },
+  {
+    label: 'Appliances',
+    value: 'appliances',
+  },
+];
+
+export default gadgets;
